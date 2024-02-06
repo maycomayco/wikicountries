c7a4eb3d03590ad010fe1dc2646ce532534980a6
Mayco Barale <maycobarale@gmail.com>
2024-02-06 11:30:07 -0300

    Refactor LeafletMapWrapper component to include country information in the popup

--- src/components/LeafletMapWrapper.tsx
+++ src/components/LeafletMapWrapper.tsx
@@ -7,16 +7,14 @@ import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import iconMarker from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { parse } from "path";
 
 const customIcon = new Icon({
   iconUrl: iconMarker.src,
   shadowUrl: iconShadow.src,
 });
 
-function MyComponent({ position }: any) {
+function MyComponent({ position, countryInfo }: any) {
   const map = useMap();
   map.setView(position);
 
@@ -28,15 +26,32 @@ function MyComponent({ position }: any) {
       />
       <Marker position={position} icon={customIcon}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          <h2 className="text-lg font-semibold">{countryInfo.name}</h2>
+          <p>
+            <span className="font-semibold">Capital: </span>
+            {countryInfo.capital}
+          </p>
+          <p>
+            <span className="font-semibold">Currency: </span>
+            {countryInfo.currency}
+          </p>
+          <p>
+            <span className="font-semibold">AWS Region: </span>
+            <span className="font-mono p-1 bg-slate-100/50 rounded">
+              {countryInfo.awsRegion}
+            </span>{" "}
+            🤓
+          </p>
         </Popup>
       </Marker>
     </>
   );
 }
 
-export default function LeafletMapWrapper({ lat, long }: any) {
+export default function LeafletMapWrapper({ lat, long, countryInfo }: any) {
   const [position, setPosition] = useState<LatLngExpression>([+lat, +long]);
+
+  const parsedCountryInfo = JSON.parse(countryInfo);
 
   useEffect(() => {
     setPosition([+lat, +long]);
@@ -50,7 +65,7 @@ export default function LeafletMapWrapper({ lat, long }: any) {
         scrollWheelZoom={false}
         className="w-full h-full"
       >
-        <MyComponent position={position} />
+        <MyComponent position={position} countryInfo={parsedCountryInfo} />
       </MapContainer>
     </div>
   );
